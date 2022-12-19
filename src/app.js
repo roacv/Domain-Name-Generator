adc6a2824c443c6237a275e8367f344afa91a456
roacv <roacv@hotmail.com>
2022-12-19 14:16:46 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".us", ".io", "etc."];
+  let domain =
+    pronoun[Math.floor(Math.random() * 2)] +
+    adj[Math.floor(Math.random() * 2)] +
+    noun[Math.floor(Math.random() * 2)] +
+    ext[Math.floor(Math.random() * 5)];
+  console.log(domain);
 };
